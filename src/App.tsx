72f13fcbd72d752fa08072303e38d9085cb539e4
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2026-05-01 21:18:40 +0530

    Setup the the frontend application and remove the unnecessery codes and the resources from the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to University Viet project</h1>
+        <p>Viet is a one of the best frontend web development tool use in the world of development.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
