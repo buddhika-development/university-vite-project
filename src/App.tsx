3d8e7b4bc15f201daf83971e53dcf7c311525cc8
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2026-05-01 21:22:03 +0530

    feat: setup the user greeting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Greeting from './components/Greeting'
 
 function App() {
   return (
     <>
       <div>
-        <h1>Welcome to University Viet project</h1>
-        <p>Viet is a one of the best frontend web development tool use in the world of development.</p>
+        <Greeting />
       </div>
     </>
   )
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,8 @@
+export default function Greeting() {
+  return (
+    <div className="">
+      <h1>Welcome to Viet project</h1>
+      <p>Viet is a one of the best frontend web development tool use in the world of development.</p>
+    </div>
+  );
+}
